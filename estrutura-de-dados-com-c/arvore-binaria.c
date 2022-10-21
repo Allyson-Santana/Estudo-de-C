91c8901324bbf4f093358c5982f1737d12afcf8d
Allyson-Santana <santana.allyson459@gmail.com>
2022-10-21 16:38:07 -0300

    config setup

---
+++ estrutura-de-dados-com-c/arvore-binaria.c
@@ -0,0 +1,16 @@
+// => https://www.youtube.com/watch?v=owaBsGQheZM
+// => https://www.youtube.com/watch?v=dcOCsxiuKeU&list=PLqJK4Oyr5WShBuC5rqt9I0fMVNnaVTpeo PlaylLst
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define ex01
+
+#ifdef ex01
+int main() {
+  printf("Hello World!");
+
+  return 0;
+}
+#endif // ex01
